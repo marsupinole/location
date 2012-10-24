74111f305b4137f5519299e36e16f912450cc229
Azam <azam@sdsol.com>
2012-10-23 20:25:43 -0400

    first

--- location/locationViewController.h
+++ location/locationViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface locationViewController : UIViewController
+@interface locationViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) IBOutlet UILabel *latitude;
+@property (strong, nonatomic) IBOutlet UILabel *longitude;
+@property (strong, nonatomic) IBOutlet UILabel *horizontalAccuracy;
+@property (strong, nonatomic) IBOutlet UILabel *verticalAccuracy;
+@property (strong, nonatomic) IBOutlet UILabel *altitude;
+@property (strong, nonatomic) IBOutlet UILabel *distance;
+@property (strong, nonatomic) CLLocation *startLocation;
+- (IBAction)resetDistance;
 
 @end
--- location/locationViewController.m
+++ location/locationViewController.m
@@ -14,10 +14,16 @@
 
 @implementation locationViewController
 
+@synthesize longitude, latitude, horizontalAccuracy, verticalAccuracy, altitude, distance, locationManager, startLocation;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    self.startLocation = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)resetDistance
+{
+    self.startLocation = nil;
+}
+
+#pragma mark -
+#pragma mark CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSString *currentLatitude = [[NSString alloc] initWithFormat:@"%+.6f", newLocation.coordinate.latitude];
+    self.latitude.text = currentLatitude;
+    NSString *currentLongitude = [[NSString alloc] initWithFormat:@"%+.6f", newLocation.coordinate.longitude];
+    self.longitude.text = currentLongitude;
+    NSString *currentHorizontalAccuracy = [[NSString alloc] initWithFormat:@"%+.6f", newLocation.horizontalAccuracy];
+    self.horizontalAccuracy.text = currentHorizontalAccuracy;
+NSString *currentAltitude  = [[NSString alloc] initWithFormat:@"%+.6f", newLocation.altitude];
+    self.altitude.text = currentAltitude;
+NSString *currentVerticalAccuracy = [[NSString alloc] initWithFormat:@"%+.6f", newLocation.verticalAccuracy];
+self.verticalAccuracy.text = currentVerticalAccuracy;
+
+if (self.startLocation == nil)
+    self.startLocation = newLocation;
+    CLLocationDistance distanceBetween = [newLocation distanceFromLocation:self.startLocation];
+    NSString *tripString = [[NSString alloc] initWithFormat:@"%f", distanceBetween];
+    self.distance.text = tripString;
+
+}
+-(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    
+}
 @end
